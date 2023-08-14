3b125a0baec5562e56b82e564381ce4ec1ab8913
Marcin <marcin.latoszek06@gmail.com>
2023-08-15 01:23:33 +0200

    added new test

--- wal-macros/tests/tests.rs
+++ wal-macros/tests/tests.rs
@@ -3,9 +3,10 @@ fn html_macro_tests() {
     let t = trybuild::TestCases::new();
     t.pass("tests/html_macro/single-value-pass.rs");
     //t.compile_fail("tests/html_macro/single-value-fail.rs");
-    //t.pass("tests/html_macro/single-element-pass.rs");
+    t.pass("tests/html_macro/single-element-pass.rs");
     //t.pass("tests/html_macro/single-value-within-element-pass.rs");
     //t.compile_fail("tests/html_macro/single-value-within-element-fail.rs");
+    //t.pass("tests/html_macro/element-tree-pass.rs");
     //t.compile_fail("tests/html_macro/element-without-closing-fail.rs");
     //t.compile_fail("tests/html_macro/element-without-opening-fail.rs");
     //t.compile_fail("tests/html_macro/element-with-wrong-way-of-closing-fail.rs");
