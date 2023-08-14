407d997c3a95d6f8b21d6653e8d4ca2341dd8134
Marcin <marcin.latoszek06@gmail.com>
2023-08-14 19:40:37 +0200

    single html value parse

--- wal-macros/src/html_macro.rs
+++ wal-macros/src/html_macro.rs
@@ -11,7 +11,7 @@ pub enum HtmlTree {
     List,
     Component,
     Element,
-    Value,
+    SingleValue(SingleHtmlValue),
 }
 
 enum HtmlType {
@@ -22,12 +22,27 @@ enum HtmlType {
     List,
     Component,
     Element,
-    Value,
+    SingleValue,
 }
 
 impl Parse for HtmlTree {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let html_type = HtmlTree::get_html_type(input);
+        let html_tree = match html_type {
+            HtmlType::Empty => Self::Empty,
+            HtmlType::SingleValue => Self::SingleValue(input.parse()?),
+            _ => unimplemented!(),
+        };
+
+        if input.is_empty() {
+            Ok(html_tree)
+        } else {
+            let remaining_stream: proc_macro2::TokenStream = input.parse()?;
+            Err(syn::Error::new_spanned(
+                remaining_stream,
+                "Unexpected tokens. Only single html root element is allowed (hint: wrap your html in '<></>')",
+            ))
+        }
     }
 }
 
@@ -47,7 +62,7 @@ impl HtmlTree {
             input.parse::<syn::token::Lt>().unwrap();
             Self::get_html_type_after_lt(&input)
         } else {
-            HtmlType::Value
+            HtmlType::SingleValue
         }
     }
 
@@ -59,7 +74,7 @@ impl HtmlTree {
         } else if input.peek(proc_macro2::Ident::peek_any) {
             Self::get_html_type_where_after_lt_is_ident(input)
         } else {
-            HtmlType::Value
+            HtmlType::SingleValue
         }
     }
 
@@ -78,6 +93,34 @@ impl HtmlTree {
             HtmlType::Component
         } else {
             HtmlType::Element
+        }
+    }
+}
+
+pub enum SingleHtmlValue {
+    Lit(syn::Lit),
+    Expr(syn::Expr),
+}
+
+impl Parse for SingleHtmlValue {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        if input.peek(syn::Lit) {
+            let lit = input.parse()?;
+
+            let error_message = match lit {
+                syn::Lit::ByteStr(_) => Some("Byte string literals are not supported"),
+                syn::Lit::Byte(_) => Some("Byte literals are not supported"),
+                syn::Lit::Verbatim(_) => Some("Unsupported literal format encountered"),
+                _ => None,
+            };
+
+            if let Some(msg) = error_message {
+                return Err(syn::Error::new_spanned(lit, msg));
+            };
+
+            Ok(Self::Lit(lit))
+        } else {
+            Ok(Self::Expr(input.parse()?))
         }
     }
 }
