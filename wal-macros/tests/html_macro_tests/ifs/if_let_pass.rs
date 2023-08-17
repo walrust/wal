d74f4c21537eec28dfe3b61bfb1a0da44542c3fc
Marcin <marcin.latoszek06@gmail.com>
2023-08-18 00:24:31 +0200

    if let tets pass

--- wal-macros/tests/html_macro_tests/ifs/if_let_pass.rs
+++ wal-macros/tests/html_macro_tests/ifs/if_let_pass.rs
@@ -1,22 +1,22 @@
 use wal_macros::html;
 
 fn main() {
-    html! { if let std::option::Option::Some(_value) = std::option::Option::Some("hello") { { "hello" } } };
-    html! { if let std::option::Option::Some(value) = std::option::Option::Some("hello") { { value } } };
+    html! { if let std::option::Option::Some(_value) = std::option::Option::Some("hello") { "hello" } };
+    html! { if let std::option::Option::Some(value) = std::option::Option::Some("hello") { value } };
     html! { if let std::option::Option::Some(value) = std::option::Option::Some("hello") { <div>{ value }</div> } };
 
     html! {
-        if let std::option::Option::Some(_value) = std::option::Option::Some("hello") { { "hello1" } }
-        else { { "hello2" } }
+        if let std::option::Option::Some(_value) = std::option::Option::Some("hello") { "hello1" }
+        else { "hello2" }
     };
     html! {
-        if let std::option::Option::Some(_value) = std::option::Option::Some("hello") { { "hello1" } }
-        else if let std::option::Option::Some(_value) = std::option::Option::Some("hello") { { "hello2" } }
+        if let std::option::Option::Some(_value) = std::option::Option::Some("hello") { "hello1" }
+        else if let std::option::Option::Some(_value) = std::option::Option::Some("hello") { "hello2" }
     };
     html! {
-        if let std::option::Option::Some(_value) = std::option::Option::Some("hello") { { "hello1" } }
-        else if let std::option::Option::Some(_value) = std::option::Option::Some("hello") { { "hello2" } }
-        else if let std::option::Option::Some(_value) = std::option::Option::Some("hello") { { "hello3" } }
+        if let std::option::Option::Some(_value) = std::option::Option::Some("hello") { "hello1" }
+        else if let std::option::Option::Some(_value) = std::option::Option::Some("hello") { "hello2" }
+        else if let std::option::Option::Some(_value) = std::option::Option::Some("hello") { "hello3" }
     };
     html! {
         if let std::option::Option::Some(_value) = std::option::Option::Some("hello") { "hello1" }
