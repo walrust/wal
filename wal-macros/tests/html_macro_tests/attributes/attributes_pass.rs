863dead5f17ee47ecac117f1767975acf013cb3f
Marcin <marcin.latoszek06@gmail.com>
2023-08-18 20:31:23 +0200

    added new cases to attribute pass

--- wal-macros/tests/html_macro_tests/attributes/attributes_pass.rs
+++ wal-macros/tests/html_macro_tests/attributes/attributes_pass.rs
@@ -6,4 +6,7 @@ fn main() {
     html! { <div key="value" other_attr1="val" other_attr2="val"/> };
     html! { <input key="value" other_attr1="val" other_attr2="val"/> };
     html! { <key="value"></> };
+    html! { <div attr={1 + 2}></div> };
+    html! { <div attr={let x = 5; x}></div> };
+    html! { <input key={"value"} other_attr1="val" other_attr2={"val"}/> };
 }
