c458c5d4f0b617c5d06e8805ebe5a71f15da0d59
Marcin <marcin.latoszek06@gmail.com>
2023-08-18 20:01:32 +0200

    attribute test - added some cases

--- wal-macros/tests/html_macro_tests/attributes/attributes_pass.rs
+++ wal-macros/tests/html_macro_tests/attributes/attributes_pass.rs
@@ -2,5 +2,8 @@ use wal_macros::html;
 
 fn main() {
     html! { <div key="value" /> };
+    html! { <div key="value" other_attr1="val" other_attr2="val"></div> };
+    html! { <div key="value" other_attr1="val" other_attr2="val"/> };
+    html! { <input key="value" other_attr1="val" other_attr2="val"/> };
     html! { <key="value"></> };
 }
