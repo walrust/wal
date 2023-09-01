6ef9ba1609a0ada8ec9ea2ce958bcd9695d1d3cf
Marcin <marcin.latoszek06@gmail.com>
2023-09-02 00:07:03 +0200

    html forest refactored

--- wal-macros/src/html_macro/html_forest.rs
+++ wal-macros/src/html_macro/html_forest.rs
@@ -18,16 +18,13 @@ impl Parse for HtmlForest {
 
 impl ToTokens for HtmlForest {
     fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
-        if self.0.len() == 1 {
-            tokens.extend(self.0[0].to_token_stream());
-        } else {
-            let html_trees = &self.0;
-
-            tokens.extend(quote! {
+        match self.0.as_slice() {
+            [single_tree] => single_tree.to_tokens(tokens),
+            trees => tokens.extend(quote! {
                 ::wal_vdom::virtual_dom::VNode::List {
-                    vlist: ::wal_vdom::virtual_dom::VList::new(vec![#(#html_trees),*])
+                    vlist: ::wal_vdom::virtual_dom::VList::new(vec![#(#trees),*])
                 }
-            });
+            }),
         }
     }
 }
