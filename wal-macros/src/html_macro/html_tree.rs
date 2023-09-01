913c780ffdf3fad6cc45eaa5444ffe530f94395b
Marcin <marcin.latoszek06@gmail.com>
2023-09-01 21:30:56 +0200

    html_tree refactor

--- wal-macros/src/html_macro/html_tree.rs
+++ wal-macros/src/html_macro/html_tree.rs
@@ -13,7 +13,7 @@ pub enum HtmlTree {
     If(HtmlIfExpression),
     For(HtmlFor<syn::ExprBlock>),
     Fragment(HtmlFragment),
-    _Component,
+    _Component, // TODO: Implement component
     Element(HtmlElement),
     Literal(HtmlLiteral),
     ExpressionBlock(syn::ExprBlock),
@@ -21,16 +21,57 @@ pub enum HtmlTree {
 
 impl Parse for HtmlTree {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        let html_type = HtmlType::get(input);
+        let html_tree = if input.peek(syn::token::If) {
+            Self::If(input.parse()?)
+        } else if input.peek(syn::token::For) {
+            Self::For(input.parse()?)
+        } else if input.peek(syn::token::Brace) {
+            Self::ExpressionBlock(input.parse()?)
+        } else if input.peek(syn::token::Lt) {
+            let forked_input = input.fork();
+            forked_input.parse::<syn::token::Lt>().unwrap();
+            forked_input.parse::<syn::token::Slash>().ok(); // parsing optional slash character for an unmatched closing tag
+            Self::parse_after_lt(input, &forked_input)?
+        } else {
+            Self::Literal(input.parse()?)
+        };
 
-        let html_tree = match html_type {
-            HtmlType::If => Self::If(input.parse()?),
-            HtmlType::For => Self::For(input.parse()?),
-            HtmlType::Fragment => Self::Fragment(input.parse()?),
-            HtmlType::Element => Self::Element(input.parse()?),
-            HtmlType::Literal => Self::Literal(input.parse()?),
-            HtmlType::ExpressionBlock => Self::ExpressionBlock(input.parse()?),
-            HtmlType::Component => unimplemented!(), // TODO: Component parsing needed
+        Ok(html_tree)
+    }
+}
+
+impl HtmlTree {
+    fn parse_after_lt(input: ParseStream, forked_input: ParseStream) -> syn::Result<Self> {
+        let html_tree = if forked_input.peek(syn::token::Gt) {
+            Self::Fragment(input.parse()?)
+        } else if forked_input.peek(syn::token::PathSep) {
+            Self::_Component // TODO: Implement parsing for component
+        } else if forked_input.peek(proc_macro2::Ident::peek_any) {
+            Self::parse_where_after_lt_is_ident(input, forked_input)?
+        } else {
+            Self::Element(input.parse()?)
+        };
+
+        Ok(html_tree)
+    }
+
+    fn parse_where_after_lt_is_ident(
+        input: ParseStream,
+        forked_input: ParseStream,
+    ) -> syn::Result<Self> {
+        let ident = proc_macro2::Ident::parse_any(forked_input)?.to_string();
+
+        let html_tree = if forked_input.peek(syn::token::Eq) {
+            Self::Fragment(input.parse()?)
+        } else if ident
+            .chars()
+            .next()
+            .map_or(false, |c| c.is_ascii_uppercase())
+            || forked_input.peek(syn::token::PathSep)
+        {
+            Self::_Component // TODO: Implement parsing for component
+        } else {
+            Self::Element(input.parse()?)
         };
 
         Ok(html_tree)
@@ -43,72 +84,12 @@ impl ToTokens for HtmlTree {
             Self::If(html_if) => html_if.to_tokens(tokens),
             Self::For(html_for) => html_for.to_tokens(tokens),
             Self::Fragment(html_fragment) => html_fragment.to_tokens(tokens),
-            Self::_Component => unimplemented!(),     // TODO: Component parsing needed
+            Self::_Component => unimplemented!(),     // TODO: implement totokens for component
             Self::Element(html_element) => html_element.to_tokens(tokens),
             Self::Literal(html_literal) => html_literal.to_tokens(tokens),
             Self::ExpressionBlock(expr_block) => tokens.extend(
                 quote_spanned!(expr_block.span() => ::wal_vdom::virtual_dom::VNode::from(#expr_block)),
             ),
-        }
-    }
-}
-
-pub enum HtmlType {
-    If,
-    For,
-    Fragment,
-    Component,
-    Element,
-    Literal,
-    ExpressionBlock,
-}
-
-impl HtmlType {
-    fn get(input: ParseStream) -> Self {
-        let input = input.fork();
-
-        if input.peek(syn::token::If) {
-            Self::If
-        } else if input.peek(syn::token::For) {
-            Self::For
-        } else if input.peek(syn::token::Brace) {
-            Self::ExpressionBlock
-        } else if input.peek(syn::token::Lt) {
-            input.parse::<syn::token::Lt>().unwrap();
-            input.parse::<syn::token::Slash>().ok(); // parsing optional slash character for unmatched closing tags
-            Self::get_after_lt(&input)
-        } else {
-            Self::Literal
-        }
-    }
-
-    fn get_after_lt(input: ParseStream) -> HtmlType {
-        if input.peek(syn::token::Gt) {
-            HtmlType::Fragment
-        } else if input.peek(syn::token::PathSep) {
-            HtmlType::Component
-        } else if input.peek(proc_macro2::Ident::peek_any) {
-            Self::get_where_after_lt_is_ident(input)
-        } else {
-            HtmlType::Element
-        }
-    }
-
-    fn get_where_after_lt_is_ident(input: ParseStream) -> HtmlType {
-        let ident = proc_macro2::Ident::parse_any(&input).unwrap();
-        let ident = ident.to_string();
-
-        if input.peek(syn::token::Eq) {
-            HtmlType::Fragment
-        } else if ident
-            .chars()
-            .next()
-            .map_or(false, |c| c.is_ascii_uppercase())
-            || input.peek(syn::token::PathSep)
-        {
-            HtmlType::Component
-        } else {
-            HtmlType::Element
         }
     }
 }
