b000aae2e367426e1fc26ea38f072300dd7dc0f8
Marcin <marcin.latoszek06@gmail.com>
2023-10-22 17:49:44 +0200

    added events hashmap

--- wal-macros/src/html_macro/mod.rs
+++ wal-macros/src/html_macro/mod.rs
@@ -1,3 +1,7 @@
+use std::collections::HashSet;
+
+use once_cell::unsync::Lazy;
+
 mod html_attribute;
 mod html_component;
 mod html_element;
@@ -12,3 +16,90 @@ mod html_tree;
 
 const KEY_STR: &str = "key";
 const PROPS_STR: &str = "props";
+
+// Events from https://www.w3schools.com/tags/ref_eventattributes.asp
+const EVENTS: Lazy<HashSet<&str>> = Lazy::new(|| {
+    [
+        // Window Event Attributes
+        "onafterprint",
+        "onbeforeprint",
+        "onbeforeunload",
+        "onerror",
+        "onhashchange",
+        "onload",
+        "onmessage",
+        "onoffline",
+        "ononline",
+        "onpagehide",
+        "onpageshow",
+        "onpopstate",
+        "onresize",
+        "onstorage",
+        "onunload",
+        // Form Events
+        "onblur",
+        "onchange",
+        "oncontextmenu",
+        "onfocus",
+        "oninput",
+        "oninvalid",
+        "onreset",
+        "onsearch",
+        "onselect",
+        "onsubmit",
+        // Keyboard Events
+        "onkeydown",
+        "onkeypress",
+        "onkeyup",
+        // Mouse Events
+        "onclick",
+        "ondblclick",
+        "onmousedown",
+        "onmousemove",
+        "onmouseout",
+        "onmouseover",
+        "onmouseup",
+        "onmousewheel",
+        "onscroll",
+        "onwheel",
+        // Drag Events
+        "ondrag",
+        "ondragend",
+        "ondragenter",
+        "ondragleave",
+        "ondragover",
+        "ondragstart",
+        "ondrop",
+        // Clipboard Events
+        "oncopy",
+        "oncut",
+        "onpaste",
+        // Media Events
+        "onabort",
+        "oncanplay",
+        "oncanplaythrough",
+        "oncuechange",
+        "ondurationchange",
+        "onemptied",
+        "onended",
+        "onerror",
+        "onloadeddata",
+        "onloadedmetadata",
+        "onloadstart",
+        "onpause",
+        "onplay",
+        "onplaying",
+        "onprogress",
+        "onratechange",
+        "onseeked",
+        "onseeking",
+        "onstalled",
+        "onsuspend",
+        "ontimeupdate",
+        "onvolumechange",
+        "onwaiting",
+        // Misc Events
+        "ontoggle",
+    ]
+    .into()
+});
