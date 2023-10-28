a21170cd9561d9ee4086ef6d220b29685c7b23a4
Marcin <marcin.latoszek06@gmail.com>
2023-10-28 14:15:42 +0200

    all events support in wal main project - macro is to be changed

--- wal/src/events/mod.rs
+++ wal/src/events/mod.rs
@@ -1,7 +1,10 @@
 use std::{borrow::Cow, fmt::Debug};
 
 use wasm_bindgen::JsCast;
-use web_sys::{Event, MouseEvent};
+use web_sys::{
+    AnimationEvent, DragEvent, Event, FocusEvent, InputEvent, KeyboardEvent, MouseEvent,
+    PointerEvent, ProgressEvent, SubmitEvent, TouchEvent, TransitionEvent, WheelEvent,
+};
 
 use crate::component::callback::Callback;
 
@@ -43,14 +46,143 @@ impl EventHandler for UnspecializedEventHandler {
 }
 
 event_handlers! {
-    MouseEventHandler(MouseEvent)
+    AnimationEventHandler(AnimationEvent),
+    DragEventHandler(DragEvent),
+    FocusEventHandler(FocusEvent),
+    InputEventHandler(InputEvent),
+    KeyboardEventHandler(KeyboardEvent),
+    MouseEventHandler(MouseEvent),
+    PointerEventHandler(PointerEvent),
+    ProgressEventHandler(ProgressEvent),
+    SubmitEventHandler(SubmitEvent),
+    TouchEventHandler(TouchEvent),
+    TransitionEventHandler(TransitionEvent),
+    WheelEventHandler(WheelEvent)
 }
 
 unspecialized_event_handlers_constructor! {
-    onabort
+    onabort,
+    oncancel,
+    oncanplay,
+    oncanplaythrough,
+    onchange,
+    onclose,
+    oncopy,
+    oncuechange,
+    oncut,
+    ondurationchange,
+    onemptied,
+    onended,
+    onerror,
+    oninvalid,
+    onload,
+    onloadeddata,
+    onloadedmetadata,
+    onpaste,
+    onpause,
+    onplay,
+    onplaying,
+    onpointerlockchange,
+    onpointerlockerror,
+    onratechange,
+    onreset,
+    onresize,
+    onscroll,
+    onsecuritypolicyviolation,
+    onseeked,
+    onseeking,
+    onselect,
+    onselectionchange,
+    onselectstart,
+    onshow,
+    onslotchange,
+    onstalled,
+    onsuspend,
+    ontimeupdate,
+    ontoggle,
+    onvolumechange,
+    onwaiting,
+
+    // FormData Events
+    onformdata  // web_sys is missing `FormDataEvent`` so it is handled as Unspecialized Event
 }
 
 event_handlers_constructor! {
+    // Animation Events
+    onanimationcancel(AnimationEvent),
+    onanimationend(AnimationEvent),
+    onanimationiteration(AnimationEvent),
+    onanimationstart(AnimationEvent),
+    
+    // Drag Events
+    ondrag(DragEvent),
+    ondragend(DragEvent),
+    ondragenter(DragEvent),
+    ondragexit(DragEvent),
+    ondragleave(DragEvent),
+    ondragover(DragEvent),
+    ondragstart(DragEvent),
+    ondrop(DragEvent),
+
+    // Focus Events
+    onblur(FocusEvent),
+    onfocus(FocusEvent),
+    onfocusin(FocusEvent),
+    onfocusout(FocusEvent),
+
+    // Input Events
+    oninput(InputEvent),
+
+    // Keyboard Events
+    onkeydown(KeyboardEvent),
+    onkeypress(KeyboardEvent),
+    onkeyup(KeyboardEvent),
+
+    // Mouse Events
+    onauxclick(MouseEvent),
     onclick(MouseEvent),
-    ondblclick(MouseEvent)
+    oncontextmenu(MouseEvent),
+    ondblclick(MouseEvent),
+    onmousedown(MouseEvent),
+    onmouseenter(MouseEvent),
+    onmouseleave(MouseEvent),
+    onmousemove(MouseEvent),
+    onmouseout(MouseEvent),
+    onmouseover(MouseEvent),
+    onmouseup(MouseEvent),
+
+    // Pointer Events
+    ongotpointercapture(PointerEvent),
+    onlostpointercapture(PointerEvent),
+    onpointercancel(PointerEvent),
+    onpointerdown(PointerEvent),
+    onpointerenter(PointerEvent),
+    onpointerleave(PointerEvent),
+    onpointermove(PointerEvent),
+    onpointerout(PointerEvent),
+    onpointerover(PointerEvent),
+    onpointerup(PointerEvent),
+
+    // Progress Events
+    onloadend(ProgressEvent),
+    onloadstart(ProgressEvent),
+    onprogress(ProgressEvent),
+
+    // Submit Events
+    onsubmit(SubmitEvent),
+
+    // Touch Events
+    ontouchcancel(TouchEvent),
+    ontouchend(TouchEvent),
+    ontouchmove(TouchEvent),
+    ontouchstart(TouchEvent),
+
+    // Transition Events
+    ontransitioncancel(TransitionEvent),
+    ontransitionend(TransitionEvent),
+    ontransitionrun(TransitionEvent),
+    ontransitionstart(TransitionEvent),
+
+    // Wheel Events
+    onwheel(WheelEvent)
 }
