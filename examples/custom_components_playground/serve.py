7b15cc256b86ef646be05a40cd8c707abc61c203
Marcin Latoszek <115318813+Marvvvin5@users.noreply.github.com>
2023-11-18 19:20:09 +0100

    Scheduler tests (#21)
    * improvement and tests for message equality and raw comparison
    
    * cmp message test
    
    * all tests written
    
    * added depth comparison
    
    * tests refactored
    
    * final test refactor

--- examples/custom_components_playground/serve.py
+++ examples/custom_components_playground/serve.py
@@ -7,7 +7,7 @@ PORT = 1337
 Handler = http.server.SimpleHTTPRequestHandler
 Handler.extensions_map.update({
       ".js": "application/javascript",
-});
+})
 
 httpd = socketserver.TCPServer(("", PORT), Handler)
 httpd.serve_forever()
