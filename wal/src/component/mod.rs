3caa35ee7d5a250235e64be5ef73494f14437895
Marcin <marcin.latoszek06@gmail.com>
2023-12-10 17:27:08 +0100

    Component comments

--- wal/src/component/mod.rs
+++ wal/src/component/mod.rs
@@ -9,16 +9,37 @@ pub mod callback;
 pub mod node;
 pub mod scheduler;
 
+/// Trait for defining custom component.
+///
+/// It adapts a MVC pattern.
+/// Model is the fields and state of the component. It is initialized by [new](#tymethod.new) function using [Properties](#associatedtype.Properties).
+/// View is the function [view](#tymethod.view) that returns a view of an application.
+/// Controller is the function [update](#tymethod.update) that updates the model based on the [Message](#associatedtype.Message).
 pub trait Component: Sized {
+    /// Type to describe the message that can be sent to the component.
+    /// It is used to update the model of the component.
     type Message: 'static;
+
+    /// Type to describe the properties that can be passed to the component.
+    /// It is used to initialize the model of the component.
     type Properties: Hash + 'static;
 
+    /// Function that creates a new instance of the component therefore initialize a model using [Properties](#associatedtype.Properties).
     fn new(props: Self::Properties) -> Self;
+
+    /// Function that returns a view of the component.
+    /// It uses [Behavior](behavior::Behavior) to create [Callbacks](callback::Callback) that are responsible to send [Messages](#associatedtype.Message) to the component.
+    /// Defining the views is done using [rsx](../../wal_rsx/macro.rsx.html) macro.
+    /// Using the macro is not mandatory but it is recommended and makes in unnecessary to know the complex structure of [VNode].
     fn view(&self, behavior: &mut impl Behavior<Self>) -> VNode;
+
+    /// Function that updates the model of the component based on the [Message](#associatedtype.Message).
+    /// It returns a boolean that indicates if the rerender of the component is necessary.
+    /// Meaning whether the view of the component should be updated or not.
     fn update(&mut self, message: Self::Message) -> bool;
 }
 
-pub trait AnyComponent {
+pub(crate) trait AnyComponent {
     fn new(props: Box<dyn Any>) -> Self
     where
         Self: Sized;
